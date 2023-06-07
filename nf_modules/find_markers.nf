4d0cfdf8e88b412992cc27b42d4d27b0d8f997da
Diego M. Coelho <diegomscoelho@gmail.com>
2023-06-07 15:44:53 -0300

    feat: Create templates usage

--- nf_modules/find_markers.nf
+++ nf_modules/find_markers.nf
@@ -9,26 +9,6 @@ process MARKERS {
     path 'Top10genes.png'
 
     script:
-    """#!/usr/bin/env Rscript
+    template 'find_markers.R'
 
-## Seurat
-library(Seurat)
-library(dplyr)
-
-pbmc <- readRDS("${rds}")
-
-pbmc.markers <- FindAllMarkers(pbmc, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-
-# Top markers
-pbmc.markers %>%
-  group_by(cluster) %>%
-  top_n(n = 10, wt = avg_log2FC) -> top10
-
-png(filename = "Top10genes.png", res = 200, width = 1600, height = 2000)
-DoHeatmap(pbmc, features = top10\$gene) + NoLegend()
-dev.off()
-
-saveRDS(pbmc, file = "pbmc3k_final.rds")
-
-    """
 } 
---
+++ templates/create.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+## Seurat
+library(Seurat)
+library(dplyr)
+
+pbmc.data <- Read10X(data.dir = "${data_dir}")
+# Initialize the Seurat object with the raw (non-normalized data).
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+
+# The [[ operator can add columns to object metadata. This is a great place to stash QC stats
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+
+# Visualize QC metrics as a violin plot
+png(filename = "VlnPlot.png", res = 200, width = 1400, height = 600)
+VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+dev.off()
+
+pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+
+saveRDS(pbmc, file = "pbmc3k.rds")
